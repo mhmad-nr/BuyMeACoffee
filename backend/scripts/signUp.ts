0483d1387335a65ec1fb7d2f3152f9a6d85201f2
mhmad-nr <83580680+mhmad-nr@users.noreply.github.com>
2024-05-05 15:24:03 +0330

    add signup script\

--- backend/scripts/signUp.ts
+++ backend/scripts/signUp.ts
@@ -1,16 +1,13 @@
 import { ethers, network } from "hardhat";
 import { address, abi } from "../deployments/sepolia/BuyMeACoffee.json";
-import {
-  ContractDeployTransaction,
-  ContractTransactionReceipt,
-  ContractTransactionResponse,
-} from "ethers";
 import { developmentChains } from "../helper-hardhat-config";
-
+import { BuyMeACoffee__factory } from "../typechain-types";
 const PRICE = ethers.parseEther("0.0000015");
 
 async function boughtNFT() {
   let MarketplaceAddress = address;
+
+  console.log(network.name);
 
   if (developmentChains.includes(network.name)) {
     MarketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
@@ -18,11 +15,18 @@ async function boughtNFT() {
 
   const buyMeACoffee = await ethers.getContractAt(abi, address);
 
-  buyMeACoffee.
+  try {
+    console.log("Signing up...");
 
-  await tx.wait(1);
+    const tx = await buyMeACoffee.singUp();
+    console.log("Waiting to be mined...");
 
-  console.log("NFT has bought...");
+    await tx.wait(1);
+
+    console.log("Successfully signed up...");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 boughtNFT()
